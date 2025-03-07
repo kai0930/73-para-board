3e0a057db9e4b20f6adb34f2f5907aabdfb584a9
kai0930 <gamo.kai.1111@gmail.com>
2025-03-07 16:43:34 +0900

    メインファイルをcppに変更

--- software/ParaBoard_esp-idf/main/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-extern "C" void app_main(void)
-{
-    gpio_reset_pin(Pins::LED);
-    gpio_set_direction(Pins::LED, GPIO_MODE_OUTPUT);
-}
---
+++ software/ParaBoard_esp-idf/main/main.cpp
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+extern "C" void app_main(void) { printf("Hello, World!\n"); }
